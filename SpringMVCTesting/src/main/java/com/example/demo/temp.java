0186d294f3d4a1f3c3e3aa44a2d4c8dfa8c39742
prakharVerma94 <prakhar.verma@oodlestechnologies.com>
2017-07-24 19:27:14 +0530

    changes in two files

--- SpringMVCTesting/src/main/java/com/example/demo/temp.java
+++ SpringMVCTesting/src/main/java/com/example/demo/temp.java
@@ -4,8 +4,11 @@ public class temp {
 	int size= 100;
 	int hiegth = 500;
 	String heloo="prakhar";
+	
 	public static void print(){
 		System.out.println("method is call for checking...");
+		System.out.println("check for changess...");
 	}
+	
 	
 }
